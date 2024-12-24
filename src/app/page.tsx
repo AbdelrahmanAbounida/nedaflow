dff206fab59c31be48e91d2f6b1680aa6381062e
DEL38800W-AAB036 <abdelrahmanaboneda@gmail.com>
2024-12-24 08:35:52 +0100

    init reactflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,37 @@
 import { Button } from "@/components/ui/button";
 import { logout } from "@/actions/auth/logout";
 import { toast } from "sonner";
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Controls,
+  ReactFlowProvider,
+  MiniMap,
+  Background,
+  BackgroundVariant,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { useCallback } from "react";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function Home() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
-    <div className="">
+    <div className=" w-full h-full">
       {/* <Button
         onClick={() => {
           toast.success("Logout");
@@ -15,7 +42,17 @@ export default function Home() {
         Logout
       </Button> */}
       {/** TODO:: Test ReactFlow */}
-      asd
+      <ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+        />
+        <Controls />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlowProvider>
       {/** TODO:: Test React Query */}
     </div>
   );
